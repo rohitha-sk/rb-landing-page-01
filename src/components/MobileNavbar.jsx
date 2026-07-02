d7b82526fb929a0bf9ad065f45ab8a738e561e47
S.K.ROHITHA <rohithasamarawickrama@gmail.com>
2026-07-03 03:47:47 +0530

    project initialization and basic layout system preparing

---
+++ src/components/MobileNavbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const MobileNavbar = () => {
+  return (
+    <div className='flex flex-row justify-between px-6 py-6'> 
+        <div className="text-gray-800 font-bold text-3xl">
+            Landy
+        </div>
+        <div>
+            Hambeger icon
+        </div>
+    </div> 
+  )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { MobileNavbar } from './MobileNavbar'
+
+const Navbar = () => {
+  return (
+    <div>
+        <MobileNavbar />
+    </div>
+  )
+}
+
+export default Navbar
